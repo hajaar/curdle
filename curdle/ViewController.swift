22c2b41ed426ae59375bf022f5bb05e3de46b543
Kartik Narayanan <hajaar@icloud.com>
2022-02-28 06:10:14 +0530

    Create Constants files Add Vocalbulary Add code to pick word at random

--- curdle/ViewController.swift
+++ curdle/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chosenWord: UILabel!
+    @IBOutlet weak var newGame: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startGame(_ sender: UIButton) {
+        chosenWord.text = K.vocalbulary.randomElement()
+    }
+    
 }
 
