67e9065dc64d7f508e42f011db0e96435b7364e8
Kartik Narayanan <hajaar@icloud.com>
2022-02-28 07:15:06 +0530

    add bunch of labels for testing logic

--- curdle/ViewController.swift
+++ curdle/ViewController.swift
@@ -8,17 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var chosenWord: UILabel!
-    @IBOutlet weak var newGame: UIButton!
+    @IBOutlet weak var label11: UILabel!
+    @IBOutlet weak var label12: UILabel!
+    @IBOutlet weak var label13: UILabel!
+    @IBOutlet weak var label14: UILabel!
+    @IBOutlet weak var label15: UILabel!
+    @IBOutlet weak var label21: UILabel!
+    @IBOutlet weak var label22: UILabel!
+    @IBOutlet weak var label23: UILabel!
+    @IBOutlet weak var label24: UILabel!
+    @IBOutlet weak var label25: UILabel!
+    @IBOutlet weak var label31: UILabel!
+    @IBOutlet weak var label32: UILabel!
+    @IBOutlet weak var label33: UILabel!
+    @IBOutlet weak var label34: UILabel!
+    @IBOutlet weak var label35: UILabel!
+    @IBOutlet weak var label41: UILabel!
+    @IBOutlet weak var label42: UILabel!
+    @IBOutlet weak var label43: UILabel!
+    @IBOutlet weak var label44: UILabel!
+    @IBOutlet weak var label45: UILabel!
+    @IBOutlet weak var label51: UILabel!
+    @IBOutlet weak var label52: UILabel!
+    @IBOutlet weak var label53: UILabel!
+    @IBOutlet weak var label54: UILabel!
+    @IBOutlet weak var label55: UILabel!
+    @IBOutlet weak var label61: UILabel!
+    @IBOutlet weak var label62: UILabel!
+    @IBOutlet weak var label63: UILabel!
+    @IBOutlet weak var label64: UILabel!
+    @IBOutlet weak var label65: UILabel!
+/* Have to replace these dumb labels with a collection view later*/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func startGame(_ sender: UIButton) {
-        chosenWord.text = K.vocalbulary.randomElement()
-    }
+
     
 }
 
