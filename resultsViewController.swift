bd9bb080ce24d69db89db468bf66bc7610244879
Kartik Narayanan <hajaar@icloud.com>
2022-03-05 08:46:35 +0530

    add Charts package. Basic radar chart working

--- resultsViewController.swift
+++ resultsViewController.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import Charts
+
 
 class resultsViewController: UIViewController {
     @IBOutlet weak var dismissButton: UIButton!
-    
     @IBOutlet weak var playedLabel: UILabel!
     @IBOutlet weak var wonLabel: UILabel!
     @IBOutlet weak var currentStreakLabel: UILabel!
     @IBOutlet weak var maxStreakLabel: UILabel!
     @IBOutlet weak var avgTimeLabel: UILabel!
+
+    @IBOutlet weak var radarChart: RadarChartView!
+    
+    
     var resultGameStats = GameStats()
     
     
@@ -28,6 +33,39 @@ class resultsViewController: UIViewController {
         currentStreakLabel.text = String(resultGameStats.currentStreak)
         maxStreakLabel.text = String(resultGameStats.maxStreak)
         avgTimeLabel.text = String(resultGameStats.avgTimeToWin)
+        
+        let greenDataSet = RadarChartDataSet(
+            entries: [
+                RadarChartDataEntry(value: 210),
+                RadarChartDataEntry(value: 60.0),
+                RadarChartDataEntry(value: 150.0),
+                RadarChartDataEntry(value: 150.0),
+                RadarChartDataEntry(value: 160.0),
+                RadarChartDataEntry(value: 150.0),
+                RadarChartDataEntry(value: 110.0),
+                RadarChartDataEntry(value: 190.0),
+                RadarChartDataEntry(value: 200.0)
+            ]
+        )
+        let redDataSet = RadarChartDataSet(
+            entries: [
+                RadarChartDataEntry(value: 120.0),
+                RadarChartDataEntry(value: 160.0),
+                RadarChartDataEntry(value: 110.0),
+                RadarChartDataEntry(value: 110.0),
+                RadarChartDataEntry(value: 210.0),
+                RadarChartDataEntry(value: 120.0),
+                RadarChartDataEntry(value: 210.0),
+                RadarChartDataEntry(value: 100.0),
+                RadarChartDataEntry(value: 210.0)
+            ]
+        )
+        
+            // 2
+        let data = RadarChartData(dataSets: [greenDataSet, redDataSet])
+        
+            // 3
+        radarChart.data = data
         
     }
     
