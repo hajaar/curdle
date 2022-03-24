49265bd44ea1c658591647e5d8f1698a894f40d7
Kartik Narayanan <1748756+hajaar@users.noreply.github.com>
2022-03-24 08:36:21 +0530

    make disable/enable keyboard based on Bool. removed function to enable/disable keyboard.

---
+++ curdle/Views/extensionTableViewDataSource.swift
@@ -0,0 +1,45 @@
+//
+//  extensionTableViewDataSource.swift
+//  curdle
+//
+//  Created by Kartik Narayanan on 24/03/22.
+//
+
+import Foundation
+import UIKit
+
+extension tableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return K.maxNumberOfAttempts
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! guessTableViewCell
+        
+        let imageArray = [cell.imageView1!, cell.imageView2!, cell.imageView3!, cell.imageView4!, cell.imageView5!]
+        
+        
+        
+        gameSession.game.getWordDetails(row: indexPath.row, typeText: tmpText.lowercased())
+        
+        for i in 0...K.maxLengthOfWord - 1 {
+            
+                // print("\(i) \(gameSession.game.wordDetails[i].letterImage)")
+            
+            UIView.transition(with: imageArray[i],
+                              duration: gameSession.game.wordDetails[i].letterDuration,
+                              options: gameSession.game.wordDetails[i].letterAnimation,
+                              animations: { imageArray[i].image = self.takeScreenShot ? K.filledTileImage : self.gameSession.game.wordDetails[i].letterImage },
+                              completion: nil)
+            
+            imageArray[i].tintColor = gameSession.game.wordDetails[i].letterColor
+            
+        }
+        
+        
+        return cell
+        
+    }
+    
+}
--- curdle/Views/tableViewController.swift
+++ curdle/Views/tableViewController.swift
@@ -122,7 +122,6 @@ class tableViewController: UIViewController {
     
     @IBAction func viewPreviousGame(_ sender: UIButton) {
         isViewingHistory = true
-        enableKeyboard(enable: false)
         nextGameButton.isEnabled = true
     }
     
@@ -131,50 +130,45 @@ class tableViewController: UIViewController {
         
     }
     
-    func enableKeyboard(enable: Bool) {
-        for item in keyButtons {
-            let tmpTag = item.tag
-            if tmpTag > 0 && tmpTag < 29 {
-                item.isEnabled = enable
-            }
-        }
-    }
+
     
     @IBAction func changeLanguage(sender: AnyObject) {
         guard let button = sender as? UIButton else {
             return
         }
-        let keyEntered: Int = button.tag
-        switch keyEntered {
-        case 27:
-            if !gameSession.game.isGameOver && tmpText.isEmpty == false {
-                tmpText.removeLast()
-                playSound("letterentry")
-                isNewGame = false
-                guessesTableView.reloadData()
-            }
-        case 28:
-            if !gameSession.game.isGameOver {
-                msgText = gameSession.game.checkGuess(guess: tmpText.lowercased())
-                playSound(gameSession.game.isValidGuess ? "wordentered" : "invalidword")
-                gameSession.setGameAttributes()
-                colorKeys()
-                guessesTableView.reloadData()
-                tmpText = ""
-            }
-            if gameSession.game.isGameOver {
-                startGameOverTasks()
-            }
-        default:
-            if tmpText.count < K.maxLengthOfWord {
-                tmpText += K.getNumberPosition(i: keyEntered - 1)
- 
-                if !gameSession.game.isGameOver {
+        if !isViewingHistory {
+            let keyEntered: Int = button.tag
+            switch keyEntered {
+            case 27:
+                if !gameSession.game.isGameOver && tmpText.isEmpty == false {
+                    tmpText.removeLast()
                     playSound("letterentry")
                     isNewGame = false
                     guessesTableView.reloadData()
                 }
-
+            case 28:
+                if !gameSession.game.isGameOver {
+                    msgText = gameSession.game.checkGuess(guess: tmpText.lowercased())
+                    playSound(gameSession.game.isValidGuess ? "wordentered" : "invalidword")
+                    gameSession.setGameAttributes()
+                    colorKeys()
+                    guessesTableView.reloadData()
+                    tmpText = ""
+                }
+                if gameSession.game.isGameOver {
+                    startGameOverTasks()
+                }
+            default:
+                if tmpText.count < K.maxLengthOfWord {
+                    tmpText += K.getNumberPosition(i: keyEntered - 1)
+                    
+                    if !gameSession.game.isGameOver {
+                        playSound("letterentry")
+                        isNewGame = false
+                        guessesTableView.reloadData()
+                    }
+                    
+                }
             }
         }
 
@@ -184,40 +178,6 @@ class tableViewController: UIViewController {
 
 
 
-extension tableViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return K.maxNumberOfAttempts
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! guessTableViewCell
-        
-        let imageArray = [cell.imageView1!, cell.imageView2!, cell.imageView3!, cell.imageView4!, cell.imageView5!]
-        
-        
-        
-        gameSession.game.getWordDetails(row: indexPath.row, typeText: tmpText.lowercased())
-        
-        for i in 0...K.maxLengthOfWord - 1 {
-            
-                // print("\(i) \(gameSession.game.wordDetails[i].letterImage)")
-            
-            UIView.transition(with: imageArray[i],
-                              duration: gameSession.game.wordDetails[i].letterDuration,
-                              options: gameSession.game.wordDetails[i].letterAnimation,
-                              animations: { imageArray[i].image = self.takeScreenShot ? K.filledTileImage : self.gameSession.game.wordDetails[i].letterImage },
-                              completion: nil)
-            
-            imageArray[i].tintColor = gameSession.game.wordDetails[i].letterColor
-            
-        }
-        
-        
-        return cell
-        
-    }
-    
-}
+
 
 
